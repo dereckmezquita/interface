6727d52defd4f1530fced54197e98c7f958beeeb
dereckmezquita <dereck@mezquita.io>
2024-07-01 17:08:52 -0500

    Moved to dev folder.

---
+++ dev/dev.R
@@ -0,0 +1,16 @@
+library('interface')
+
+Person <- interface("Person",
+    name = "character",
+    age = "numeric",
+    email = "character",
+    validate_on_access = TRUE
+)
+
+john <- implement(Person,
+    name = "John Doe",
+    age = 30,
+    email = "john@example.com"
+)
+
+print(john)
