71945be4efba6cb008f8443143fa6f0f2e950441
Dereck Mezquita <dereck@mezquita.io>
2024-07-26 20:28:29 -0500

    Working interface and typed function.

--- R/fun.R
+++ R/fun.R
@@ -1,30 +1,42 @@
 #' Create a typed function
 #'
-#' @param args A list of argument names and their expected types
+#' @param ... Named arguments defining the function parameters and their types
 #' @param return The expected return type(s)
 #' @param impl The function implementation
 #' @return A typed function
 #' @export
-fun <- function(args, return, impl) {
-  force(args)
+fun <- function(..., return, impl) {
+  args <- list(...)
   force(return)
   force(impl)
+
+  # Ensure that 'return' and 'impl' are not in the args list
+  args <- args[!names(args) %in% c("return", "impl")]
 
   typed_fun <- function(...) {
     call_args <- list(...)
     
     # Validate input arguments
     for (arg_name in names(args)) {
-      if (!(arg_name %in% names(call_args))) {
+      if (arg_name %in% names(call_args)) {
+        arg_value <- call_args[[arg_name]]
+        arg_type <- args[[arg_name]]
+        
+        error <- validate_property(arg_name, arg_value, arg_type)
+        if (!is.null(error)) {
+          stop(error, call. = FALSE)
+        }
+      } else if (length(call_args) >= which(names(args) == arg_name)) {
+        # If the argument wasn't named, but a value was provided in the correct position
+        arg_value <- call_args[[which(names(args) == arg_name)]]
+        arg_type <- args[[arg_name]]
+        
+        error <- validate_property(arg_name, arg_value, arg_type)
+        if (!is.null(error)) {
+          stop(error, call. = FALSE)
+        }
+      } else {
         stop(sprintf("Missing required argument: %s", arg_name), call. = FALSE)
-      }
-      
-      arg_value <- call_args[[arg_name]]
-      arg_type <- args[[arg_name]]
-      
-      error <- validate_property(arg_name, arg_value, arg_type)
-      if (!is.null(error)) {
-        stop(error, call. = FALSE)
       }
     }
     
