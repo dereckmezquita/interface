e81c9658882690eca8b5aa852fde55c165f532de
Dereck Mezquita <dereck@mezquita.io>
2024-07-26 23:42:15 -0500

    Code formatted.

--- R/validate_property.R
+++ R/validate_property.R
@@ -12,61 +12,61 @@
 #' If the property value does not meet the criteria, an error message is returned.
 #'
 validate_property <- function(name, value, validator) {
-  if (is.list(validator) && !is.function(validator)) {
-    # Multiple allowed types
-    for (v in validator) {
-      error <- validate_property(name, value, v)
-      if (is.null(error)) return(NULL)
-    }
-    return(sprintf("Property '%s' does not match any of the allowed types", name))
-  } else if (inherits(validator, "interface")) {
-    if (!inherits(value, "interface_object") || !identical(attr(value, "properties"), attr(validator, "properties"))) {
-      return(sprintf("Property '%s' must be an object implementing the specified interface", name))
-    }
-  } else if (is.function(validator)) {
-    if (identical(validator, character)) {
-      if (!is.character(value)) {
-        return(sprintf("Property '%s' must be of type character", name))
-      }
-    } else if (identical(validator, numeric)) {
-      if (!is.numeric(value)) {
-        return(sprintf("Property '%s' must be of type numeric", name))
-      }
-    } else if (identical(validator, logical)) {
-      if (!is.logical(value)) {
-        return(sprintf("Property '%s' must be of type logical", name))
-      }
-    } else if (identical(validator, integer)) {
-      if (!is.integer(value)) {
-        return(sprintf("Property '%s' must be of type integer", name))
-      }
-    } else if (identical(validator, double)) {
-      if (!is.double(value)) {
-        return(sprintf("Property '%s' must be of type double", name))
-      }
-    } else if (identical(validator, complex)) {
-      if (!is.complex(value)) {
-        return(sprintf("Property '%s' must be of type complex", name))
-      }
-    } else if (identical(validator, data.table::data.table)) {
-      if (!data.table::is.data.table(value)) {
-        return(sprintf("Property '%s' must be a data.table", name))
-      }
+    if (is.list(validator) && !is.function(validator)) {
+        # Multiple allowed types
+        for (v in validator) {
+            error <- validate_property(name, value, v)
+            if (is.null(error)) return(NULL)
+        }
+        return(sprintf("Property '%s' does not match any of the allowed types", name))
+    } else if (inherits(validator, "interface")) {
+        if (!inherits(value, "interface_object") || !identical(attr(value, "properties"), attr(validator, "properties"))) {
+            return(sprintf("Property '%s' must be an object implementing the specified interface", name))
+        }
+    } else if (is.function(validator)) {
+        if (identical(validator, character)) {
+            if (!is.character(value)) {
+                return(sprintf("Property '%s' must be of type character", name))
+            }
+        } else if (identical(validator, numeric)) {
+            if (!is.numeric(value)) {
+                return(sprintf("Property '%s' must be of type numeric", name))
+            }
+        } else if (identical(validator, logical)) {
+            if (!is.logical(value)) {
+                return(sprintf("Property '%s' must be of type logical", name))
+            }
+        } else if (identical(validator, integer)) {
+            if (!is.integer(value)) {
+                return(sprintf("Property '%s' must be of type integer", name))
+            }
+        } else if (identical(validator, double)) {
+            if (!is.double(value)) {
+                return(sprintf("Property '%s' must be of type double", name))
+            }
+        } else if (identical(validator, complex)) {
+            if (!is.complex(value)) {
+                return(sprintf("Property '%s' must be of type complex", name))
+            }
+        } else if (identical(validator, data.table::data.table)) {
+            if (!data.table::is.data.table(value)) {
+                return(sprintf("Property '%s' must be a data.table", name))
+            }
+        } else {
+            # Custom validator function
+            validation_result <- vapply(value, validator, logical(1))
+            if (!all(validation_result)) {
+                invalid_indices <- which(!validation_result)
+                return(sprintf("Invalid value(s) for property '%s' at index(es): %s", name, paste(invalid_indices, collapse = ", ")))
+            }
+        }
+    } else if (is.character(validator)) {
+        if (!inherits(value, validator)) {
+            return(sprintf("Property '%s' must be of type %s, but got %s", name, validator, class(value)[1]))
+        }
     } else {
-      # Custom validator function
-      validation_result <- vapply(value, validator, logical(1))
-      if (!all(validation_result)) {
-        invalid_indices <- which(!validation_result)
-        return(sprintf("Invalid value(s) for property '%s' at index(es): %s", name, paste(invalid_indices, collapse = ", ")))
-      }
+        return(sprintf("Invalid validator for property '%s'", name))
     }
-  } else if (is.character(validator)) {
-    if (!inherits(value, validator)) {
-      return(sprintf("Property '%s' must be of type %s, but got %s", name, validator, class(value)[1]))
-    }
-  } else {
-    return(sprintf("Invalid validator for property '%s'", name))
-  }
-  
-  return(NULL)
+
+    return(NULL)
 }
